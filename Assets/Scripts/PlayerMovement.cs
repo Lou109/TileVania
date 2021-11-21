7a0b8b5fbf76c798600f6c38f373bccbebfbd224
Louise Collins <louisecollins1971@icloud.com>
2021-11-21 10:29:08 +0000

    working on cinemamachine

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -37,9 +37,10 @@ public class PlayerMovement : MonoBehaviour
         FlipSprite();
         ClimbLadder();
         Die();
+        Wait();
     }
 
-    void FixedUpdate()
+    void Wait()
     {
         if (transform.parent != null && transform.parent.tag == "Player")
         {
