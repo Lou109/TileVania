67d6b3402d05cc51092f5ce18dac5cab46452652
Louise Collins <louisecollins1971@icloud.com>
2022-01-06 04:11:56 +0000

    sorted out coin volume problem

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
+    [Range(0f, 1f)][SerializeField] float volume = 0.2f;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position, volume);
             Destroy(gameObject);
         }    
     }
