3c7f86f76b9db4ec244ae8a1a1ee1b85165a75be
Louise Collins <louisecollins1971@icloud.com>
2022-01-26 16:15:21 +0000

    105. Persistent Score & Lives

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -6,12 +6,18 @@ public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
     [Range(0f, 1f)][SerializeField] float volume = 0.2f;
+    [SerializeField] int pointsForCoinPickup = 100;
+
+    bool wasCollected = false;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !wasCollected)
         {
+            wasCollected = true;
+            FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position, volume);
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }    
     }
